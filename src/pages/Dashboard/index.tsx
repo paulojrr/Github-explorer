5b2cd26f156e17291be3de9c00cc27d83509dda3
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2021-03-14 02:33:42 -0300

    estilizando dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,63 @@
 import React from 'react';
+import { FiChevronsRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const DashBoard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="#">
+          <img
+            src="https://avatars.githubusercontent.com/u/50791278?s=460&u=c4b7992193215dc3b6c703a7da39690a2fc5a488&v=4"
+            alt="Paulo"
+          />
+          <div>
+            <strong>paulojrr/API-NodeJS</strong>
+            <p>Api Nodejs</p>
+          </div>
+
+          <FiChevronsRight size={20} />
+        </a>
+
+        <a href="#">
+          <img
+            src="https://avatars.githubusercontent.com/u/50791278?s=460&u=c4b7992193215dc3b6c703a7da39690a2fc5a488&v=4"
+            alt="Paulo"
+          />
+          <div>
+            <strong>paulojrr/API-NodeJS</strong>
+            <p>Api Nodejs</p>
+          </div>
+
+          <FiChevronsRight size={20} />
+        </a>
+
+        <a href="#">
+          <img
+            src="https://avatars.githubusercontent.com/u/50791278?s=460&u=c4b7992193215dc3b6c703a7da39690a2fc5a488&v=4"
+            alt="Paulo"
+          />
+          <div>
+            <strong>paulojrr/API-NodeJS</strong>
+            <p>Api Nodejs</p>
+          </div>
+
+          <FiChevronsRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default DashBoard;
