787d905572bc29f6cc3288d86958c7261b48ad3a
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2021-03-14 22:41:59 -0300

    estilizando detalhes

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronsLeft, FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
   repository: string;
@@ -8,7 +13,55 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
-  return <h1>Repository:{params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronsLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars.githubusercontent.com/u/50791278?s=460&u=c4b7992193215dc3b6c703a7da39690a2fc5a488&v=4"
+            alt="xxx"
+          />
+          <div>
+            <strong>rocket/unform</strong>
+            <p>xxxxxxx</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1800</strong>
+            <span>starts</span>
+          </li>
+          <li>
+            <strong>1800</strong>
+            <span>starts</span>
+          </li>
+          <li>
+            <strong>1800</strong>
+            <span>starts</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="dasda">
+          <div>
+            <strong>ddsadsad</strong>
+            <p>dsddsd</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
